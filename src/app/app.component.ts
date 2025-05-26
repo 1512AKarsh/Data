cc177b6d169e8dbd61aab0be45889c543904e174
Akarsh Singh <vanshgangwar903@gmail.com>
2025-05-26 14:50:54 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,65 +11,16 @@ import { ProfileComponent } from './profile/profile.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  // title = 'Akarsh Singh';
-  //    x = 10;
-  //    y = 20;
-  //   user1 = 'Akarsh Singh';
-  //   user2 = 'Aman Singh'
-  // Education = 'B.Tech in Computer Science and Engineering';
-  // hello(){
-  //   let x = 20;
-  // }
+ handleEvent(event:MouseEvent){
+  // console.log("Function Called", event.type);
+  // console.log("function called",(event.target as Element).classList);
+ // console.log("function called",event.target.name);
+  console.log("function called",event.target);
+ }
+handleInputEvent(event:Event){
+  
+console.log("function called", event.type);
+console.log("function called", (event.target as HTMLInputElement).value);
 
-  handleClickEvent(){
-    console.log("Button Clicked");
-    this.otherFunction();
-
-  }
-  otherFunction(){
-    console.log("Other Function Called");
-  }
-
-  name:string ="Akarsh Singh";
-  data:number | string= 10;
-  other:any = true;
-  updateName(){
-    this.name = "Ayush Pal";
-  this.data = 20;
-  this.data = "Akarsh Singh";
-  this.other = false;
-  console.log(this.name);
-}
-updateVar(){
-  let x:number | boolean = 30;
-  console.log(x);
-  this.sum(10,40);
-}
-
-sum(a:number,b:number){
-  console.log(a + b) ;
-}
-
-
-count:number = 0;
-// incrementCount(){
-// this.count++;
-// }
-// decrementCount(){
-//   this.count--;}
-// resetCount(){
-// this.count = 0;
-// }
-
-
-handleCounter(val:string){
-  if(val == 'plus'){
-    this.count++;
-  }else if(val == 'minus'){
-    if(this.count > 0)
-    this.count--;
-  }else if(val === 'reset'){
-    this.count =0;
-  }
 }
 }
